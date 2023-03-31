3133da75e06bbe90105b8f33a1359d51348a6e4a
macbear <macbear@qq.com>
2023-03-31 23:48:01 +0800

    backup from 2023-03-31

--- scripts/commit.ts
+++ scripts/commit.ts
@@ -1,6 +1,15 @@
 import { execSync as exec } from 'child_process';
 import dayjs from 'dayjs';
 
+const name = "macbear"
+const email = "macbear@qq.com"
+
+const currentName = exec('git config user.name', { encoding: 'utf-8' }).trim();
+const currentEmail = exec('git config user.email', { encoding: 'utf-8' }).trim();
+
+currentName !== name && exec(`git config user.name "${name}"`, { stdio: 'inherit' });
+currentEmail !== email && exec(`git config user.email "${email}"`, { stdio: 'inherit' });
+
 exec('git add .', { stdio: 'inherit' });
 
 exec(`git commit -m "backup from ${dayjs().format('YYYY-MM-DD')}"`, { stdio: 'inherit' });
